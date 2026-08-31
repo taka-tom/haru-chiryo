c2f0654a3d02f656b3fba340f455fbb935f7eca3
taka-tom <takatomo.takatomo.01@gmail.com>
2026-08-31 16:09:34 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function selectCourse(course) {
   courseSelect.value = course;
 
 }
+
+const dateInput =
+  document.getElementById("date");
+
+const today = new Date();
+
+today.setDate(today.getDate() + 3);
+
+const year = today.getFullYear();
+
+const month =
+  String(today.getMonth() + 1).padStart(2, "0");
+
+const day =
+  String(today.getDate()).padStart(2, "0");
+
+dateInput.min =
+  `${year}-${month}-${day}`;
